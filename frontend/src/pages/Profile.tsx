573b9bbb0a882b31c0cb07d919cfdc3a50fa9226
Dima <shesterikov02@mail.ru>
2025-04-26 12:35:20 +0300

    update profile

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,22 +1,19 @@
 import { useAuth } from "../hooks/useAuth";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
+import FatSecretButton from '../components/FatSecretButton';
 
 export default function Profile () {
     const { user } = useAuth()
-    const navigate = useNavigate()
+    //const navigate = useNavigate()
 
-    
     return (
-        <div className="bg-gray-50 py-8">
-            <div className="max-w-3xl mx-auto px-4">
-                <div className="bg-white rounded-xl shadow-md">
-                    <div className="bg-gradient-to-r from-blue-400 to-blue">
-
-                    </div>
-                </div>
-
+        <div>
+            <p>{user?.username}</p>
+            
+            <div className="mt-6 border-t pt-4">
+            <h3 className="text-lg font-medium">Интеграции</h3>
+            <FatSecretButton />
             </div>
-
         </div>
     )
 }
